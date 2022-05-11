ea2a4b251ff45fb1af9379cf0d21669cb97058c1
Novia Phan <noviaphan@gmail.com>
2022-05-10 22:40:53 -0400

    adding the first prompt of questions

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
 
+const Employee = require ("./library/Employee")
 const Manager = require ("./library/Manager")
+const Enginner = require ("./library/Engineer")
+const Intern =require ('./library/Intern')
+
+const generatePage = require("./src/html")
+const team = [];
+
 // prompt
+
+function managerInfo() {
+    const questions = [
+        {
+            type:"input",
+            message: "What is the name of the team manager?",
+            name:"name"
+        },
+        {
+            type: "input",
+            message: "What is the Manager's office number?",
+            name: "number",
+        },
+        {
+            type: "input",
+            message: "What is the employee ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the employee email adress?",
+            name: "email"
+        },
+    
+    ];
+}
 
 // inquire.prompt
 
---
+++ src/html.js
@@ -0,0 +1,5 @@
+const Employee = require ("../library/Employee")
+const Manager = require ("../library/Manager")
+const Engineer = require("../library/Engineer")
+const Intern = require("../library/Intern")
+
